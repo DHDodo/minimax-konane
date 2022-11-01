a3246490ae9afc7b26aec8da9c7e69375af9d3b3
DHDodo <bumpmytoast@gmail.com>
2022-11-01 01:16:56 -0400

    Corrected minimax with temp eval function

--- MinimaxPlayer.py
+++ MinimaxPlayer.py
@@ -1,3 +1,6 @@
+## Filename: MinimaxPlayer.py
+## Authors: Briar Sauble and Robert Bunning
+
 from konane import *
 
 # float("inf") or -float("inf") indicate no more moves
@@ -6,25 +9,25 @@ class MinimaxPlayer(Konane, Player):
     def __init__(self, size, depthLimit):
         Konane.__init__(self, size)
         self.limit = depthLimit
-        self.name = "Unique team name"
+
+    def initialize(self, side):
+        self.side = side
+        self.name = "Super Minimax Deluxe"
 
     # The initial call will be for the maximizing player
     # Node is a board
-    def minimax(node, depth, alpha, beta, isMaximizingPlayer):
-        allValidMoves = self.generateMoves(node, self.side)
-        # If depth is zero or we are the deepest in the tree
-        if depth == 0 || allValidMoves.len() == 0:
+    def minimax(self, node, side, depth, alpha, beta, isMaximizingPlayer):
+        allValidMoves = self.generateMoves(node, side)
+        # If depth is zero or we are the deepest in the tree, get heuristic
+        if depth == 0 or len(allValidMoves) == 0:
             # return the heuristic of this node
-            return eval(node)
+            return self.eval(node)
         if isMaximizingPlayer:
-            value = -float("inf")
-            # get possible moves for the node and FOR EACH
-            #   Value = the max b/w value and a recursive call to the alpha beta:
-            #   alphabeta(child, depth - 1, alpha, beta, false)
-            #    alpha = max(alpha, value)
-            #    This is inside of the loop
+            value = float("-inf")
+            # get possible moves for the node 
+            # Value = the max b/w value and a recursive call to the alpha beta:
             for move in allValidMoves:
-                value = max(value, alphabeta(self.nextBoard(node, self.side, move), depth - 1, alpha, beta, False))
+                value = max(value, self.minimax(self.nextBoard(node, side, move), self.opponent(self.side), depth - 1, alpha, beta, False))
                 if value >= beta:
                     break # beta cutoff
                 alpha = max(alpha, value)
@@ -34,38 +37,38 @@ class MinimaxPlayer(Konane, Player):
             value = float("inf")
             # get possible moves for the node
             # Value = the min b/w value and a recursive call to the alpha beta:
-            # alphabeta(child, depth - 1, alpha, beta, true)
-            # beta = min(beta, value)
             for move in allValidMoves:
-                value = min(value, alphabeta(self.nextBoard(node, self.opponent(self.side), move), depth - 1, alpha, beta, True))
+                value = min(value, self.minimax(self.nextBoard(node, side, move), self.side, depth - 1, alpha, beta, True))
                 if value <= alpha:
                     break # alpha cutoff
                 beta = min(alpha, value)
             return value
-            
-
-    def initialize(self, side):
-        pass
 
     def getMove(self, board):
         moves = self.generateMoves(board, self.side)
         n = len(moves)
+        # If there's no moves, don't bother
         if n == 0:
             return []
         else:
-            bestMove = (-float("inf"), null)
+            # We want to find the best next move
+            bestMove = [float("-inf"), None]
+            # Loop through all possible moves for our maximizer
             for move in moves:
-                moveWeight = minimax(board, self.limit, -float("inf"), float("inf"), True)
-                if (moveWeight >= bestMove[0])
+                # Calculate the weight of the move based on what our minimizing opponent will do
+                moveWeight = self.minimax(self.nextBoard(board, self.side, move), self.opponent(self.side), self.limit, float("-inf"), float("inf"), False)
+                # Replace the best move if it obtains a higher score
+                if (moveWeight >= bestMove[0]):
                     bestMove[0] = moveWeight
-                     bestMove[1] = move
+                    bestMove[1] = move
             return bestMove[1]
                 
     # 
     def eval(self, board):
-        pass
+        # We don't care what this does yet, just make it eval something as a test
+        return random.randint(0, 9)
+
 
 game = Konane(8)
-game.playOneGame(RandomPlayer(8), RandomPlayer(8), 1)
-
-# game.playNGames(2, MinimaxPlayer(8, 2), MinimaxPlayer(8, 1), 0)
+# game.playOneGame(RandomPlayer(8), RandomPlayer(8), 1)
+game.playNGames(2, MinimaxPlayer(8, 2), MinimaxPlayer(8, 1), 0)
